4f1c927c9c5ad4b9cbabfe1b6926cf599c340135
Lucas Gómez (KB) <79912231+Pigamer37@users.noreply.github.com>
2024-12-01 11:47:00 +0100

    Finally getting requests from Stremio
    Defined own methods for parsing extra arguments and only processing movies atm.
    
    Now onto the letterboxd API

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 const express = require("express")
 const app = express()
 
-const fsPromises = require("fs/promises")
+const { addonBuilder, serveHTTP, publishToCentral }  = require('stremio-addon-sdk')
 
+function setCORS(req, res, next) {
+  //console.log(req.originalUrl);
+  res.header(`Access-Control-Allow-Origin`, `*`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  next();
+}
+app.use(setCORS);
+
+const fsPromises = require("fs/promises")
 app.get("/manifest.json", (req, res) => {
   fsPromises.readFile('./package.json', 'utf8').then((data) => {
     const packageJSON = JSON.parse(data);
@@ -23,7 +33,7 @@ app.get("/manifest.json", (req, res) => {
       }
     }
 
-    res.json({
+    manifest = {
       "id": 'com.' + packageJSON.name.replaceAll('-', '.'),
       "version": packageJSON.version,
       "name": humName,
@@ -42,44 +52,38 @@ app.get("/manifest.json", (req, res) => {
         "tt"
       ],
       "logo": "https://www.stremio.com/website/stremio-logo-small.png"
-    });
-  }).catch((err) => res.status(500).statusMessage("Error reading file"));
-})
+    }
+    res.json(manifest);
+  })/*.then(()=>{
+    const builder = new addonBuilder({
+      id: manifest.id,
+      version: manifest.version,
+      name: manifest.name,
+      catalogs: [],
+      resources: manifest.resources,
+      types: manifest.types,
+      idPrefixes: manifest.idPrefixes
+    })
 
-app.get("/:userId/subtitles/:type/:videoID.json", (req, res) => {
-  console.log('bruh');
-  res.json({subtitles: []});
-})
-app.get("/:userId/subtitles/:type/:videoID/:videoHash.json", (req, res) => {
-  console.log('bruh');
-  res.json({subtitles: []});
+    builder.defineSubtitlesHandler(function(args) {
+      /*if (args.id === 'tt1254207') {
+          // serve one subtitle
+          const subtitle = {
+              url: 'https://mkvtoolnix.download/samples/vsshort-en.srt',
+              lang: 'eng'
+          }
+          return Promise.resolve({ subtitles: [subtitle] })
+      } else {
+          // otherwise return no subtitles
+          return Promise.resolve({ subtitles: [] })
+      }*/
+      /*console.log(`sub request at supposed: ${args.type}/${args.id}/${args?.extra?.videoHash}.json`)
+      return Promise.resolve({ subtitles: [] })
+    })
+  })*/.catch((err) => res.status(500).statusMessage("Error reading file"));
 })
 
 const subtitles = require("./routes/subtitles");
 app.use('/subtitles', subtitles);
 
-const setCORS = (req, res, next) => {
-  console.log(req.originalUrl);
-  res.header(`Access-Control-Allow-Origin`, `*`);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  next();
-};
-
-app.use(setCORS);
-
 app.listen(3000);
-/*const { addonBuilder, serveHTTP, publishToCentral }  = require('stremio-addon-sdk')
-const builder = new addonBuilder({
-  id: 'org.myexampleaddon',
-  version: packageJSON.version,
-
-  name: 'simple example',
-
-  // Properties that determine when Stremio picks this addon
-  // this means your addon will be used for streams of the type movie
-  catalogs: [],
-  resources: ['stream'],
-  types: ['movie'],
-  idPrefixes: ['tt']
-})*/
--- routes/subtitles.js
+++ routes/subtitles.js
@@ -3,8 +3,15 @@ const subtitles = express.Router()
 
 require('dotenv').config()//process.env.var
 
+function HandleLongSubRequest(req, res, next) {
+  console.log(`e\x1b[96mEntered HandleLongSubRequest with\x1b[39m ${req.originalUrl}`)
+  const params = searchParamsRegex(req.params[0])
+  next()
+}
 function HandleSubRequest(req, res, next) {
-  if(req.params.type!=="movie"){
+  console.log(`\x1b[96mEntered HandleSubRequest with\x1b[39m ${req.originalUrl}`)
+  if(req.params.type==="movie"){
+    console.log('\x1b[33mGot a movie\x1b[39m')
     res.json({subtitles: []});
     next();
     return;
@@ -12,9 +19,23 @@ function HandleSubRequest(req, res, next) {
   res.json({subtitles: []});
 }
 
-subtitles.get("/:type/:videoId/:videoHash.json", HandleSubRequest)
+subtitles.get("/:type/:videoId/*.json", HandleLongSubRequest, HandleSubRequest)
 subtitles.get("/:type/:videoId.json", HandleSubRequest)
-subtitles.get("/:type/:videoId/:videoHash.json", HandleSubRequest)
-subtitles.get("/:type/:videoId.json", HandleSubRequest)
+
+function searchParamsRegex(extraParams) {
+  //console.log(`\x1b[33mfull extra params were:\x1b[39m ${extraParams}`)
+  if(extraParams!==undefined){
+    const paramMap = new Map()
+    const keyVals = extraParams.split('&');
+    for(keyVal of keyVals){
+      const keyValArr = keyVal.split('=')
+      const param = keyValArr[0]; const val = keyValArr[1];
+      paramMap.set(param, val)
+    }
+    const paramJSON = Object.fromEntries(paramMap)
+    console.log(paramJSON)
+    return paramJSON
+  }else return {}
+}
 
 module.exports=subtitles;
